be18ae00f7353701c44150d1fdd6afde18c1c752
turcuciprian <turcuciprian1@gmail.com>
2017-12-30 20:04:21 +0200

    initial push

---
+++ ect.php
@@ -0,0 +1,10 @@
+<?php 
+
+    /*
+	    Plugin Name: Easy Contdown Timer
+	    Plugin URI: http://www.ciprianturcu.com
+	    Description: A very easy to use and intuitive countdown timer for wordpress
+	    Author: Ciprian Turcu
+	    Version: 1.0.0
+	    Author URI: http://www.ciprianturcu.com
+    */
